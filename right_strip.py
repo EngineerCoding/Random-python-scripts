53819cd9993dddce77fb53545b2d5cb996bfabc4
Wesley A <EngineerCoding@hotmail.nl>
2019-10-02 09:16:29 +0200

    Rename the name

--- right_strip.py
+++ right_strip.py
@@ -58,7 +58,7 @@ def right_strip_file(file_path: Union[Path, str]) -> str:
     temporary_file = NamedTemporaryFile(delete=False)
     with file_path.open('r') as file_in, temporary_file as file_out:
         right_strip(file_in, _BinaryFileWrapper(file_out))
-    os.rename(temporary_file, str(file_path))
+    os.rename(temporary_file.name, str(file_path))
     return 'success'
 
 
